b3c832374440bbd170ff12a1191c1993949dccfb
Brendan Manning <BrendanManning19380@gmail.com>
2016-09-06 13:59:24 -0400

    Create NumberType.java

---
+++ Number-Conversion-Swing/NumberType.java
@@ -0,0 +1,14 @@
+
+/**
+ * Interface instead of enum so we can read the string value for the dropdown box
+ * 
+ * @author (Brendan Manning) 
+ * @version (9/6/16)
+ */
+public interface NumberType
+{
+    String BIN = "Binary";
+    String OCT = "Octal";
+    String DEC = "Decimal";
+    String HEX = "Hexadecimal";
+}
